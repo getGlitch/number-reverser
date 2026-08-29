edaba18e0d6eca43166c7764a09c718dde640467
Kunal Nailwal <nailwalkunal007@gmail.com>
2026-08-29 03:03:08 -0700

    Fixed erraform formatting locally

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -26,8 +26,8 @@ resource "helm_release" "kyverno" {
   repository = "https://kyverno.github.io/kyverno/"
   chart      = "kyverno"
   version    = "3.9.0"
-  wait    = true
-  timeout = 600
+  wait       = true
+  timeout    = 600
 
   depends_on = [module.eks]
 }
