ea7e83055369992f74b6704573f79297d51b6b26
Kunal Nailwal <nailwalkunal007@gmail.com>
2026-08-29 06:41:15 -0700

    Permissions added for the role

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,7 +66,25 @@ module "eks" {
   endpoint_public_access  = true
   endpoint_private_access = true
 
+  #  enable_cluster_creator_admin_permissions = true
+
   enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+    github_actions = {
+      principal_arn = "arn:aws:iam::598907064200:role/GitHubActions-NumberReverser"
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
 
   addons = {
     vpc-cni = {
