bbfce0ed0bb698d6d1919c26b2ff3f8c346003a9
Kunal Nailwal <nailwalkunal007@gmail.com>
2026-08-29 05:12:01 -0700

    Final pipeline

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -21,13 +21,12 @@ provider "helm" {
 resource "helm_release" "kyverno" {
   name             = "kyverno"
   namespace        = "kyverno"
-  create_namespace = true
-
-  repository = "https://kyverno.github.io/kyverno/"
-  chart      = "kyverno"
-  version    = "3.9.0"
-  wait       = true
-  timeout    = 600
+  create_namespace = false
+  repository       = "https://kyverno.github.io/kyverno/"
+  chart            = "kyverno"
+  version          = "3.9.0"
+  wait             = true
+  timeout          = 300
 
   depends_on = [module.eks]
 }
